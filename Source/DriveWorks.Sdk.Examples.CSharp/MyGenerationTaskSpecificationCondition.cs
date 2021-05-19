7629af7ec5612ecbe9b9a469220906f4320d2b42
Thomas Guillard <thomasguillard@driveworks.co.uk>
2021-05-19 16:21:27 +0100

    Including CSharp namespace

--- Source/DriveWorks.Sdk.Examples.CSharp/MyGenerationTaskSpecificationCondition.cs
+++ Source/DriveWorks.Sdk.Examples.CSharp/MyGenerationTaskSpecificationCondition.cs
@@ -1,29 +1,32 @@
 ﻿using DriveWorks.Components.Tasks;
 using DriveWorks.Specification;
 
-/// <summary>
-/// A Generation Task Specification Condition which determines whether the value entered is even.
-/// </summary>
-/// <remarks><see cref="ComponentTaskReleaseCondition"/> are evaluated at the time of releasing component-sets.</remarks>
-[ComponentTaskCondition("Is Even",
-                        "Passes if the specified value is even.",
-                        "embedded://DriveWorks.Sdk.Examples.CSharp.Puzzle-16x16.png",
-                        "SDK-Starter-Examples Plugin")]
-public class MyGenerationTaskSpecificationCondition : ComponentTaskReleaseCondition
+namespace DriveWorks.Sdk.Examples.CSharp
 {
-    private readonly FlowProperty<double> mValue;
-
-    public MyGenerationTaskSpecificationCondition()
+    /// <summary>
+    /// A Generation Task Specification Condition which determines whether the value entered is even.
+    /// </summary>
+    /// <remarks><see cref="ComponentTaskReleaseCondition"/> are evaluated at the time of releasing component-sets.</remarks>
+    [ComponentTaskCondition("Is Even",
+                            "Passes if the specified value is even.",
+                            "embedded://DriveWorks.Sdk.Examples.CSharp.Puzzle-16x16.png",
+                            "SDK-Starter-Examples Plugin")]
+    public class MyGenerationTaskSpecificationCondition : ComponentTaskReleaseCondition
     {
-        mValue = this.Properties.RegisterDoubleProperty("Value",
-                                                        "The value to check for must be an even number for the condition to pass.");
-    }
+        private readonly FlowProperty<double> mValue;
 
-    protected override bool Evaluate(SpecificationContext specificationContext)
-    {
-        // If a number is evenly divisible by 2 with no remainder, then it is even.
-        var isEven = (mValue.Value % 2) == 0;
+        public MyGenerationTaskSpecificationCondition()
+        {
+            mValue = this.Properties.RegisterDoubleProperty("Value",
+                                                            "The value to check for must be an even number for the condition to pass.");
+        }
 
-        return isEven;
+        protected override bool Evaluate(SpecificationContext specificationContext)
+        {
+            // If a number is evenly divisible by 2 with no remainder, then it is even.
+            var isEven = (mValue.Value % 2) == 0;
+
+            return isEven;
+        }
     }
 }
