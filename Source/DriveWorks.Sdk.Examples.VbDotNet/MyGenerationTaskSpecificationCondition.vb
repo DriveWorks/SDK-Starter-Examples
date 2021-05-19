163b24d8bdd274194ddf721f2072a75804ee4694
Thomas Guillard <thomasguillard@driveworks.co.uk>
2021-05-19 12:38:50 +0100

    Adding a VB Generation Task Specification Condition and fixing image.
    This introduces an example of a Generation Task Specification Condition written in VB but also fixes the puzzle icon image not being embedded quite right.

---
+++ Source/DriveWorks.Sdk.Examples.VbDotNet/MyGenerationTaskSpecificationCondition.vb
@@ -0,0 +1,26 @@
+﻿Imports DriveWorks.Components.Tasks
+Imports DriveWorks.Specification
+
+''' <summary>
+''' A Generation Task Specification Condition which determines whether the value entered is even.
+''' </summary>
+''' <remarks><see cref="ComponentTaskReleaseCondition"/> are evaluated at the time of releasing component-sets.</remarks>
+<ComponentTaskCondition("Is Even",
+                        "Passes if the specified value is even.",
+                        "embedded://DriveWorks.Sdk.Examples.VbDotNet.Puzzle-16x16.png",
+                        "SDK-Starter-Examples Plugin")>
+Public Class MyGenerationTaskSpecificationCondition
+    Inherits ComponentTaskReleaseCondition
+
+    Private ReadOnly mValue As FlowProperty(Of Double) = Me.Properties.RegisterDoubleProperty("Value",
+                                                                                              "The value to check for must be an even number for the condition to pass.")
+
+    Protected Overrides Function Evaluate(specificationContext As SpecificationContext) As Boolean
+
+        ' If a number is evenly divisible by 2 with no remainder, then it is even.
+        Dim isEven = (mValue.Value Mod 2) = 0
+
+        Return isEven
+
+    End Function
+End Class
