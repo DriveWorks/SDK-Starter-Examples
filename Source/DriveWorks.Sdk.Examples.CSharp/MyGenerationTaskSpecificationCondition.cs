133085a23fc8efcb4612a48687688b42a55488ca
Thomas Guillard <thomasguillard@driveworks.co.uk>
2021-05-19 12:37:05 +0100

    Adding a C# Generation Task Specification Condition.
    This introduces an example of a Generation Task Specification Condition written in C#.

---
+++ Source/DriveWorks.Sdk.Examples.CSharp/MyGenerationTaskSpecificationCondition.cs
@@ -0,0 +1,29 @@
+﻿using DriveWorks.Components.Tasks;
+using DriveWorks.Specification;
+
+/// <summary>
+/// A Generation Task Specification Condition which determines whether the value entered is even.
+/// </summary>
+/// <remarks><see cref="ComponentTaskReleaseCondition"/> are evaluated at the time of releasing component-sets.</remarks>
+[ComponentTaskCondition("Is Even",
+                        "Passes if the specified value is even.",
+                        "embedded://DriveWorks.Sdk.Examples.CSharp.Puzzle-16x16.png",
+                        "SDK-Starter-Examples Plugin")]
+public class MyGenerationTaskSpecificationCondition : ComponentTaskReleaseCondition
+{
+    private readonly FlowProperty<double> mValue;
+
+    public MyGenerationTaskSpecificationCondition()
+    {
+        mValue = this.Properties.RegisterDoubleProperty("Value",
+                                                        "The value to check for must be an even number for the condition to pass.");
+    }
+
+    protected override bool Evaluate(SpecificationContext specificationContext)
+    {
+        // If a number is evenly divisible by 2 with no remainder, then it is even.
+        var isEven = (mValue.Value % 2) == 0;
+
+        return isEven;
+    }
+}
