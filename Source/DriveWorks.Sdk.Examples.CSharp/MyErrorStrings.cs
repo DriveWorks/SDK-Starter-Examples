4f5104d3bebe5efad3457ab666fe16adc2444266
Mindaugas Jarmaliunas <MindaugasJarmaliunas@driveworks.co.uk>
2021-05-26 13:45:48 +0100

    Update

--- Source/DriveWorks.Sdk.Examples.CSharp/MyErrorStrings.cs
+++ Source/DriveWorks.Sdk.Examples.CSharp/MyErrorStrings.cs
@@ -5,13 +5,14 @@ namespace DriveWorks.Sdk.Examples.CSharp
 {
     public class ExampleErrorStrings : SharedProjectExtender
     {
-        private const string EXAMPLE_ERROR_STRINGS = "#FINDUSERID! User ID invalid.";
+        private const string EXAMPLE_ERROR_STRING = "#FINDUSERID! User ID invalid.";
+
         /// <remarks>
         /// UDF validates User ID input.
-        /// If User ID is 3 characters in length function will output successful message otherwise an error. 
+        /// If User ID is 3 characters in length function will output successful message otherwise an error string. 
         /// </remarks>   
         [Udf(true)]
-        [FunctionInfo("ExampleErrorString.", "SDK-Starter-Examples Plugin")]
+        [FunctionInfo("This function returns an error string if the user ID supplied is not 3 characters in length. Error strings can be caught in rules using IfErrorString() or IsErrorString().", "SDK-Starter-Examples Plugin")]
         public object ExampleErrorString([ParamInfo("User ID", "Enter User ID.")] string userID)
         {
             if (userID.Length == 3)
@@ -20,7 +21,7 @@ namespace DriveWorks.Sdk.Examples.CSharp
             }
             else
             {
-                return EXAMPLE_ERROR_STRINGS;
+                return EXAMPLE_ERROR_STRING;
             }
         }
     }
--- Source/DriveWorks.Sdk.Examples.VbDotNet/MyErrorStrings.vb
+++ Source/DriveWorks.Sdk.Examples.VbDotNet/MyErrorStrings.vb
@@ -4,13 +4,13 @@ Imports Titan.Rules.Execution
 Public Class MyErrorStrings
     Inherits ProjectExtender
 
-    Private Const CONTENT_LENGTH_ERROR_PREFIX = "#FINDUSERID! User ID invalid."
+    Private Const EXAMPLE_ERROR_STRING = "#FINDUSERID! User ID invalid."
     '<remarks>
     'UDF validates User ID input.
-    ' If User ID Is 3 characters In length Function will output successful message otherwise an Error. 
+    ' If User ID Is 3 characters In length Function will output successful message otherwise an error string. 
     '</remarks>   
     <Udf(True)>
-    <FunctionInfo("ExampleErrorString.", "SDK-Starter-Examples Plugin")>
+    <FunctionInfo("This function returns an error string if the user ID supplied is not 3 characters in length. Error strings can be caught in rules using IfErrorString() or IsErrorString().", "SDK-Starter-Examples Plugin")>
     Function ExampleErrorString(<ParamInfo("User ID", "Enter User ID integer.")> ByVal userID As String) As Object
 
         If (userID.Length = 3) Then
@@ -19,7 +19,7 @@ Public Class MyErrorStrings
 
         Else
 
-            Return CONTENT_LENGTH_ERROR_PREFIX
+            Return EXAMPLE_ERROR_STRING
 
         End If
     End Function
